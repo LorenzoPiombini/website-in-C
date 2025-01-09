f5e81319f986ce26c1d323429d8de64a6d48e35a
LorenzoPiombini <piombinilorenzo1987@gmail.com>
2025-01-09 14:53:36 -0500

    add some content

--- src/pages.c
+++ src/pages.c
@@ -6,7 +6,7 @@
 
 #define NAV_BAR "<nav class=\"navbar navbar-expand-lg bg-body-tertiary\">\n\
 		<div class=\"container-fluid\">\n\
-		<a class=\"navbar-brand\" href=\"\\#\">Lorenzo Piombini</a>\n\
+		<a class=\"navbar-brand\">Lorenzo Piombini</a>\n\
 		<button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"\\#navbarNavAltMarkup\" aria-controls=\"navbarNavAltMarkup\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n\
 		<span class=\"navbar-toggler-icon\"></span>\n\
 		</button>\n\
@@ -25,14 +25,14 @@
 	     <h5 class=\"card-title\">Isam database</h5>\n\
 	     <p class=\"card-text\">This is a filesystem database,\
 			inspired from the custom software\
-			of the company that I am currently working at</p>\n\
+			of the company that I am currently working at.</p>\n\
 		    <a href=\"https://github.com/LorenzoPiombini/isam.db-C-language.git\" class=\"btn btn-primary\">Look at the Code</a>\n"
 
 #define CARD_CONT_b "<img src=\"user-3331257_1280.png\" class=\"card-img-top\" alt=\"...\">\n\
 			<div class=\"card-body\">\n\
 			<h5 class=\"card-title\">Libuser</h5>\n\
 			<p class=\"card-text\">A small library to add user programmatically,\
-			in your C programs, without exposing the shell or using fork()</p>\n\
+			in your C programs, without exposing the shell or using fork().</p>\n\
 			<a href=\"https://github.com/LorenzoPiombini/libuser.git\" class=\"btn btn-primary\">Look at the Code</a>\n"
 
 #define CARD_CONT_c "<img src=\"memory-8141642_1280.jpg\" class=\"card-img-top\" alt=\"...\">\n\
@@ -40,7 +40,7 @@
 			<h5 class=\"card-title\">Mem safe</h5>\n\
 			<p class=\"card-text\">A library to make C a safer language,\
 			I'd like for C to have some cool feature like Rust, and I am working on a library\
-			that could henance memory safety in C</p>\n\
+			that could henance memory safety in C.</p>\n\
 		    <a href=\"\\#\" class=\"btn btn-primary\">Look at the Code</a>\n"
 
 #define CARD_HEAD "<div class=\"card\" style=\"width: 18rem;\">\n"
@@ -99,10 +99,14 @@ int index_html(char** page)
                          </head>\n\
                          </body>\n"\
 			 NAV_BAR\
-                         "<h1>#include&ltWelcome!!.h&gt</h1>\n\
-                         <h3>This is a website entiring made with the C programming language!</h3>\
-			 <a href=\"https://github.com/LorenzoPiombini/website-in-C.git\">click here to see the repo </a>\
-			 <div class=\"container mt-5\">\n\
+                         "<h1 class=\"text-center\">#include&ltWelcome!!.h&gt</h1>\n\
+                         <h3 class=\"text-center\">This is a website entiring made with the C programming language!</h3>\
+                         <h3 class=\"text-center\">Well, kind of! Let me explain ...</h3><br>\
+                         <h3 class=\"text-center\">everything you see on this page is loaded via</h3><br>\
+                         <h3 class=\"text-center\"> a C program on a droplet, the software is a \"custom\" HTTP server</h3><br>\
+                         <h3 class=\"text-center\">that receive request from browsers and send back the response! </h3><br>\
+                         <h3 class=\"text-center\">there is no html files, everything is C strings \"callocated\" and sent to this browser.</h3><br>\
+                         <h3 class=\"text-center\"><<a href=\"https://github.com/LorenzoPiombini/website-in-C.git\">click here to see the repo </a>\/h3><br>\
 			 <div class=\"row justify-content-center\">\n\
 			 <div class=\"col-md-4\">\n"\
 			 CARD_HEAD\
