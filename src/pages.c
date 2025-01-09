250f4a270d931bfc974de0d776d6e9b6d844f92d
LorenzoPiombini <piombinilorenzo1987@gmail.com>
2025-01-09 11:43:51 -0500

    added NAV_BAR

--- src/pages.c
+++ src/pages.c
@@ -3,7 +3,21 @@
 #include <string.h>
 #include "pages.h"
 
-
+#define NAV_BAR "<nav class=\"navbar navbar-expand-lg bg-body-tertiary\">\n\
+		<div class=\"container-fluid\">\n\
+		<a class=\"navbar-brand\" href=\"\#\">Lorenzo Piombini</a>\n\
+		<button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"\#navbarNavAltMarkup\" aria-controls=\"navbarNavAltMarkup\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n\
+		<span class=\"navbar-toggler-icon\"></span>\n\
+		</button>\n\
+		<div class=\"collapse navbar-collapse\" id=\"navbarNavAltMarkup\">\n\
+		<div class=\"navbar-nav\">\n\
+		<a class=\"nav-link active\" aria-current=\"page\" href=\"/\">Home</a>\n\
+		<a class="nav-link" href="/about">About</a>\n\
+		<a class="nav-link" href="/contact">Contact</a>\n\
+		</div>\n\
+		 </div>\n\
+		</div>\n\
+		</nav>\n"
 
 
 int index_html(char** page)
@@ -23,8 +37,9 @@ int index_html(char** page)
                          }\n\
                          </style>\n\
                          </head>\n\
-                         </body>\n\
-                         <h1>#include&ltWelcome!!.h&gt</h1>\n\
+                         </body>\n"\
+			 NAV_BAR\
+                         "<h1>#include&ltWelcome!!.h&gt</h1>\n\
                          <h3>This is a website entiring made with the C programming language!</h3>\
 			 <a href=\"https://github.com/LorenzoPiombini/website-in-C.git\">click here to see the repo </a>\
 			  <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz\" crossorigin=\"anonymous\"></script>\
