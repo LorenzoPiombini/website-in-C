f91f19ac0e65a806f87b2cd706c6f4085b7f3330
LorenzoPiombini <piombinilorenzo1987@gmail.com>
2025-01-09 12:44:49 -0500

    added pics

--- src/pages.c
+++ src/pages.c
@@ -19,24 +19,30 @@
 		</div>\n\
 		</nav>\n"
 
-#define CARD_CONT_a "<p class=\"card-text\">This is a filesystem database,\
+#define CARD_CONT_a "<img src=\"./pexels-cookiecutter-1148820.jpg\" class=\"card-img-top\" alt=\"...\">\n\
+	     <div class=\"card-body\">\n\
+	     <h5 class=\"card-title\">Isam database</h5>\n\
+	     <p class=\"card-text\">This is a filesystem database,\
 			inspired from the custom software\
 			of the company that I am currently working at</p>\n\
 		    <a href=\"https://github.com/LorenzoPiombini/isam.db-C-language.git\" class=\"btn btn-primary\">Look at the Code</a>\n"
 
-#define CARD_CONT_b "<p class=\"card-text\">A small library to add user programmatically,\
+#define CARD_CONT_b "<img src=\"user-3331257_1280.png\" class=\"card-img-top\" alt=\"...\">\n\
+			<div class=\"card-body\">\n\
+			<h5 class=\"card-title\">Libuser</h5>\n\
+			<p class=\"card-text\">A small library to add user programmatically,\
 			in your C programs, without exposing the shell or using fork()</p>\n\
-		    <a href=\"https://github.com/LorenzoPiombini/libuser.git\" class=\"btn btn-primary\">Look at the Code</a>\n"
+			<a href=\"https://github.com/LorenzoPiombini/libuser.git\" class=\"btn btn-primary\">Look at the Code</a>\n"
 
-#define CARD_CONT_c "<p class=\"card-text\">A library to make C a safer language,\
+#define CARD_CONT_c "<img src=\"memory-8141642_1280.jpg\" class=\"card-img-top\" alt=\"...\">\n\
+			<div class=\"card-body\">\n\
+			<h5 class=\"card-title\">Mem safe</h5>\n\
+			<p class=\"card-text\">A library to make C a safer language,\
 			I'd like for C to have some cool feature like Rust, and I am working on a library\
 			that could henance memory safety in C</p>\n\
 		    <a href=\"\\#\" class=\"btn btn-primary\">Look at the Code</a>\n"
 
-#define CARD_HEAD "<div class=\"card\" style=\"width: 18rem;\">\n\
-	     <img src=\"...\" class=\"card-img-top\" alt=\"...\">\n\
-	     <div class=\"card-body\">\n\
-	     <h5 class=\"card-title\">Card title</h5>\n"
+#define CARD_HEAD "<div class=\"card\" style=\"width: 18rem;\">\n"
 
 #define CARD_TAIL "</div>\n</div>\n"
 int index_html(char** page)
